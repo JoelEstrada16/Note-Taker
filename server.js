2a897792073c6312358316e15eb44a0eb8773eb3
Joel Estrada <joeljazzhands@Joels-MBP.attlocal.net>
2023-05-14 07:15:42 -0700

    cleaned up the code

--- server.js
+++ server.js
@@ -2,24 +2,21 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/apiroutes.js');
 
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 
 const app = express();
 
 
-// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
 app.use(express.static('public'));
 
-// GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
